af5f32a28bba12679728b55027c42e5aaa46952a
michelmanzatotebar <zsouta22@gmail.com>
2026-06-10 19:20:02 -0400

    Estrutura spring boot

---
+++ src/test/java/com/michel/gerenciamento/demandas/DemandasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michel.gerenciamento.demandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
